75b5dd9e01716c3a9d1c35f2fb933667504e901e
Theo <theo-ng@users.noreply.github.com>
2015-10-27 10:46:13 -0700

    refactored

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,26 @@
-1.upto(100) do |i|
-  if i % 5 == 0 && i % 3 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  else
-    puts i
+def fizzbuzz(start, end_num)
+  start.upto(end_num) do |i|
+      puts "FizzBuzz" if fizz(i) && buzz(i)
+      puts "Buzz" if buzz(i) && !fizz(i)
+      puts "Fizz" if fizz(i) && !buzz(i)
+      puts i unless (fizz(i) || buzz(i))
   end
 end
+
+def buzz(num)
+  if num % 5 == 0
+    true
+  else 
+    false
+  end
+end
+
+def fizz(num)
+  if (num % 3 == 0)
+    true
+  else 
+    false
+  end
+end
+
+fizzbuzz(1,100)
